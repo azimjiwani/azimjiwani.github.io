217d0671b0134f93c9bc05584e31cf53a17e2d6c
Azim Jiwani <azimjiwani@hotmail.com>
2018-10-28 21:52:20 -0400

    changed fonts and style

--- contact-form.php
+++ contact-form.php
@@ -7,9 +7,9 @@
 <body style="text-align: center;padding:20px 0;">
 	<?php
 
-		$name = $_POST['contactname'];
-		$email = $_POST['contactemail'];
-		$message = $_POST['contactmessage'];
+		$name = $_POST['contact_name'];
+		$email = $_POST['contact_email'];
+		$message = $_POST['contact_message'];
 		
 		$formcontent=" From: Mim Contact Form $email \n
 		Name: $name \n
